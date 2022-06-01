e10c7073324cdaf0b2908ad0dce6b234ac815ebb
MikeGajardo <mikegajardo@gmail.com>
2022-06-01 03:15:24 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,20 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let fullUrl = [];
+  let pronoun = ["the", "our"];
+  let adjective = ["awesome", "gigantic"];
+  let noun = ["skater", "bulldog"];
+  let domain = [".com", ".org", ".eth"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adjective.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < domain.length; l++) {
+          fullUrl.push(pronoun[i] + adjective[j] + noun[k] + domain[l]);
+          console.log(fullUrl[fullUrl.length - 1]);
+        }
+      }
+    }
+  }
 };
